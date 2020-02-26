2c004ba90172cd9fba39fe7d7778be620fbfcd2d
Chenwe_i_lin <1846913566@qq.com>
2020-02-26 22:44:40 +0800

    released: v0.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="kuriyama",
-    version='0.1.4',
+    version='0.1.5',
     description='A framework for OICQ(QQ, made by Tencent) headless client "Mirai".',
     author='Chenwe-i-lin',
     author_email="Chenwe_i_lin@outlook.com",
