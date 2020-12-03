32ba7ab5f2d3f4129d6051dd97622c5493ddb22e
Chentang1202 <75363147+Chentang1202@users.noreply.github.com>
2020-12-03 16:08:54 +0800

    Update logger.py
    time zone fix

--- mirai/logger.py
+++ mirai/logger.py
@@ -5,6 +5,9 @@ from logbook import (
 )
 import sys
 
+import logbook
+logbook.set_datetime_format("local")
+
 stream_handler = StreamHandler(sys.stdout, level=INFO)
 stream_handler.format_string = '[{record.time:%Y-%m-%d %H:%M:%S}][Mirai] {record.level_name}: {record.channel}: {record.message}'
 stream_handler.push_application()
